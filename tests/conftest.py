f7514128db686ddbcdde44ef2d93964e268c2984
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-06-20 11:45:55 +0300

    fix conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,3 +20,26 @@ def second_product():
         price=210000.0,
         quantity=8
     )
+
+@pytest.fixture
+def third_product():
+    return Product(
+        name="Xiaomi Redmi Note 11",
+        description="1024GB, Синий",
+        price=31000.0,
+        quantity=14
+    )
+
+@pytest.fixture
+def first_category(first_product, second_product, third_product):
+    return Category(
+        name="Смартфоны",
+        description="Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        products=[first_product, second_product, third_product],
+    )
+
+@pytest.fixture(autouse=True)
+def reset_category_counts():
+    """Сброс глобальных счётчиков категорий и продуктов перед каждым тестом."""
+    Category.category_count = 0
+    Category.product_count = 0
