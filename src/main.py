e905f832b32a74ef41d07e06701a0f4ee868877b
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-06-23 22:27:19 +0300

    fix all files after mypy and add htmlcov

--- src/main.py
+++ src/main.py
@@ -1,23 +1,21 @@
+from typing import Optional, Dict, List
+
+
 class Product:
     """Класс для создания продуктов"""
 
-    name: str
-    description: str
-    price: float
-    quantity: int
-
-    def __init__(self, name, description, price, quantity):
+    def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
         self.name = name
         self.description = description
         self.__price = price
         self.quantity = quantity
 
     @property
-    def price(self):
+    def price(self) -> float:
         return self.__price
 
     @price.setter
-    def price(self, new_price):
+    def price(self, new_price: float) -> None:
         if new_price <= 0:
             print("Цена не должна быть нулевая или отрицательная")
         elif new_price < self.__price:
@@ -31,32 +29,39 @@ class Product:
             self.__price = new_price
 
     @classmethod
-    def new_product(cls, data: dict, existing_products: list = None):
-        """Создаёт новый товар или обновляет существующий по имени"""
-        existing_products = existing_products or []
+    def new_product(cls, data: dict) -> "Product":
+        """Создаёт новый товар из словаря данных"""
+        name = str(data.get("name", ""))
+        description = str(data.get("description", ""))
+        price = float(data.get("price", 0.0))
+        quantity = int(data.get("quantity", 0))
 
-        name = data.get("name")
-        description = data.get("description")
-        price = data.get("price")
-        quantity = data.get("quantity")
+        return cls(name, description, price, quantity)
+
+    @staticmethod
+    def merge_products(new_data: Dict, existing_products: List["Product"]) -> "Product":
+        """Проверяет наличие продукта с таким же именем и обновляет его, если найден. Иначе — создаёт новый."""
+        name = str(new_data.get("name", ""))
+        description = str(new_data.get("description", ""))
+        price = float(new_data.get("price", 0.0))
+        quantity = int(new_data.get("quantity", 0))
 
         for product in existing_products:
             if product.name == name:
                 product.quantity += quantity
                 product.price = max(product.price, price)
                 return product
-        return cls(name, description, price, quantity)
+
+        return Product(name, description, price, quantity)
+
 
 class Category:
     """Класс для создания категорий"""
 
-    name: str
-    description: str
-    products: list
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name: str, description: str, products: Optional[list[Product]] = None) -> None:
         self.name = name
         self.description = description
         self.__products = products if products else []
@@ -64,10 +69,10 @@ class Category:
         Category.product_count += len(products) if products else 0
 
     @property
-    def products(self):
+    def products(self) -> list:
         return self.__products
 
-    def add_product(self, product):
+    def add_product(self, product: Product) -> None:
         if isinstance(product, Product):
             self.__products.append(product)
             Category.product_count += 1
@@ -75,83 +80,77 @@ class Category:
             raise TypeError("Можно добавлять только объекты класса Product")
 
     @property
-    def product_list(self):
-        return [f'{product.name}, {int(product.price)} руб. Остаток: {product.quantity} шт.'
-        for product in self.__products]
+    def product_list(self) -> list:
+        return [
+            f"{product.name}, {int(product.price)} руб. Остаток: {product.quantity} шт." for product in self.__products
+        ]
 
 
 if __name__ == "__main__":  # pragma: no cover
-    # product1 = Product(
-    #     "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
-    # )
-    # product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    # product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-    #
-    # print(product1.name)
-    # print(product1.description)
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    print(product1.name)
+    print(product1.description)
     # print(product1.price)
-    # print(product1.quantity)
-    #
-    # print(product2.name)
-    # print(product2.description)
+    print(product1.quantity)
+
+    print(product2.name)
+    print(product2.description)
     # print(product2.price)
-    # print(product2.quantity)
-    #
-    # print(product3.name)
-    # print(product3.description)
+    print(product2.quantity)
+
+    print(product3.name)
+    print(product3.description)
     # print(product3.price)
-    # print(product3.quantity)
-    #
-    # category1 = Category(
-    #     "Смартфоны",
-    #     "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-    #     [product1, product2, product3],
-    # )
-    #
-    # print(category1.name == "Смартфоны")
-    # print(category1.description)
-    # print(len(category1.products))
-    # print(category1.category_count)
-    # print(category1.product_count)
-    #
-    # product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
-    # category2 = Category(
-    #     "Телевизоры",
-    #     "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-    #     [product4],
-    # )
-    #
-    # print(category2.name)
-    # print(category2.description)
-    # print(len(category2.products))
-    # print(category2.products)
-    #
-    # print(Category.category_count)
-    # print(Category.product_count)
-    #
-    # cat = Category("Смартфоны", "Описание", [product1, product2])
-    # for line in cat.product_list:
-    #     print(line)
-    #
-    # p1 = Product("Iphone 15", "512GB, Gray", 210000.0, 5)
-    # products = [p1]
-    #
-    # new_data = {
-    #     "name": "Iphone 15",
-    #     "description": "512GB, Gray",
-    #     "price": 215000.0,
-    #     "quantity": 3
-    # }
-    #
-    # updated_product = Product.new_product(new_data, products)
-    #
-    # # Добавим в список, если нового не было
-    # if updated_product not in products:
-    #     products.append(updated_product)
-    #
-    # # Вывод
-    # for p in products:
-    #     print(f"{p.name}, {p.price} руб., Остаток: {p.quantity} шт.")
+    print(product3.quantity)
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+
+    print(category1.name == "Смартфоны")
+    print(category1.description)
+    print(len(category1.products))
+    print(category1.category_count)
+    print(category1.product_count)
+
+    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    category2 = Category(
+        "Телевизоры",
+        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        [product4],
+    )
+
+    print(category2.name)
+    print(category2.description)
+    print(len(category2.products))
+    print(category2.products)
+
+    print(Category.category_count)
+    print(Category.product_count)
+
+    cat = Category("Смартфоны", "Описание", [product1, product2])
+    for line in cat.product_list:
+        print(line)
+
+    p1 = Product("Iphone 15", "512GB, Gray", 210000.0, 5)
+    products = [p1]
+
+    new_data = {"name": "Iphone 15", "description": "512GB, Gray", "price": 215000.0, "quantity": 3}
+
+    updated_product = Product.merge_products(new_data, products)
+
+    # Добавим в список, если нового не было
+    if updated_product not in products:
+        products.append(updated_product)
+
+    # Вывод
+    for p in products:
+        print(f"{p.name}, {p.price} руб., Остаток: {p.quantity} шт.")
 
     product = Product("Телевизор", "4K QLED", 50000, 10)
 
